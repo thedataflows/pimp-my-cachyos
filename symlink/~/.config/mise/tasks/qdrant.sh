68c94d2b0e6f96eba35972a80f546ee499f86579
Cristian Chiru <cristi.chiru@gmail.com>
2025-10-20 13:16:55 +0300

    update mise qdrant script

--- symlink/~/.config/mise/tasks/qdrant.sh
+++ symlink/~/.config/mise/tasks/qdrant.sh
@@ -1,3 +1,21 @@
 #!/bin/env bash
 
-docker run -p 6333:6333 -p 6334:6334 -v "/mnt/linux2/dev/ai-storage/qdrant_storage:/qdrant/storage:z" qdrant/qdrant:latest
+CONTAINER_NAME=qdrant
+
+CONTAINER_STATE_CMD="docker ps --all --format json | jq -r 'select(.Names==\"$CONTAINER_NAME\").State'"
+CONTAINER_STATE=$(eval $CONTAINER_STATE_CMD)
+if [[ -n "$CONTAINER_STATE" ]]; then
+  if [[ "$CONTAINER_STATE" != "running" ]]; then
+    set -x
+    docker start "$CONTAINER_NAME"
+    { set +x; } &>/dev/null
+  fi
+  CONTAINER_STATE=$(eval $CONTAINER_STATE_CMD)
+  echo $CONTAINER_NAME $CONTAINER_STATE
+  exit 0
+fi
+
+set -x
+docker run -d --name "$CONTAINER_NAME" -p 6333:6333 -p 6334:6334 -v "/mnt/linux2/dev/ai-storage/qdrant_storage:/qdrant/storage:z" qdrant/qdrant:latest
+sleep 1
+echo $CONTAINER_NAME $CONTAINER_STATE
