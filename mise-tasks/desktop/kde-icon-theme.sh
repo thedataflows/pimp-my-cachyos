048c4337626d35940d0348502b58fde8d7f3e2eb
Cristian Chiru <cristi.chiru@gmail.com>
2026-01-18 14:58:54 +0200

    replace sddm with plasma-login

--- mise-tasks/desktop/kde-icon-theme.sh
+++ mise-tasks/desktop/kde-icon-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-#MISE description="Set the same icon theme for all users (including sddm and root)"
+#MISE description="Set the same icon theme for all users (current user and root)"
 
 set -Eeuo pipefail
 trap 'echo "[ERROR] on line $LINENO: \"${BASH_COMMAND}\" exited with status $?"' ERR
@@ -7,7 +7,7 @@ trap 'echo "[ERROR] on line $LINENO: \"${BASH_COMMAND}\" exited with status $?"'
 paru -Q qogir-icon-theme &> /dev/null || $PARU qogir-icon-theme
 
 CMD="kwriteconfig6 --file kdeglobals --group Icons --key Theme Qogir-Dark"
-for U in $USER sddm root; do
+for U in $USER root; do
   set -x
   # shellcheck disable=SC2086
   sudo -u $U $CMD
