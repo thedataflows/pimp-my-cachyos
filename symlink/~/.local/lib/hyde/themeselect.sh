8488f94b2a402738676912a11d7b5f932f2005f6
Cristian Chiru <cristi.chiru@gmail.com>
2026-01-31 20:03:08 +0200

    remove hyde

--- symlink/~/.local/lib/hyde/themeselect.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-scrDir="$(dirname "$(realpath "$0")")"
-"${scrDir}"/theme.select.sh "$@"
--- symlink/~/.local/lib/hyde/themeswitch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "This script will be deprecated. Please use theme.switch.sh instead."
-scrDir="$(dirname "$(realpath "$0")")"
-"${scrDir}"/theme.switch.sh "$@"
