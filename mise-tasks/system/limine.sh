a6f2db35e80a691dcab39438a9882e5cf4c8c200
Cristian Chiru <cristi.chiru@gmail.com>
2026-06-07 21:55:21 +0300

    limine: fix script, deps

--- mise-tasks/system/limine.sh
+++ mise-tasks/system/limine.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 #MISE description="Limine configuration"
+#MISE interactive=true
 
 set -Eeuo pipefail
 trap 'echo "[ERROR] on line $LINENO: \"${BASH_COMMAND}\" exited with status $?"' ERR
