68b5943be5349a3202fc191e826608539e841ba1
Cristian Chiru <cristi.chiru@gmail.com>
2026-06-12 19:03:40 +0300

    add env-reload

---
+++ mise-tasks/env-reload.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+#MISE description="Reload current user environment variables and update dbus activation environment"
+
+set -a
+for f in ~/.config/environment.d/*.conf; do
+  #shellcheck disable=SC1090
+  source "$f"
+done
+set +a
+
+set -x
+systemctl --user import-environment
+dbus-update-activation-environment --all
+type niri &>/dev/null && niri msg action load-config-file || true
