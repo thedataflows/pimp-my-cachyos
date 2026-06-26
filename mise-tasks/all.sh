78cca19abc7ebb3afeb30e014685639227fc1df7
Cristian Chiru <cristi.chiru@gmail.com>
2026-06-26 20:24:02 +0300

    mise-tasks: interactive all.sh script

--- mise-tasks/all.sh
+++ mise-tasks/all.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 #MISE description="Run all tasks in the proper order"
+#MISE interactive=true
 
 set -Eeuo pipefail
 trap 'echo "[ERROR] on line $LINENO: \"${BASH_COMMAND}\" exited with status $?"' ERR
